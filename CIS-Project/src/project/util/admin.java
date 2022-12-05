1b9121a53b6b46e9f90763d538be278dbec86be3
nicolettepowell <nicolettepowell@192.168.0.168>
2022-12-04 19:38:10 -0500

    all classes

---
+++ CIS-Project/src/project/util/admin.java
@@ -0,0 +1,14 @@
+package project.util;
+
+public class admin extends User {
+
+	public admin( String username, String password, String userType, String firstName, String lastName,
+			String address, String city, String state, String zipCode, String email, String ssn, 
+			String securityQuestion, String securityAnswer) {
+		
+		super(username, password, userType, firstName, lastName, address, city, state, zipCode, email, ssn,
+				securityQuestion, securityAnswer);
+	}
+	
+
+}
