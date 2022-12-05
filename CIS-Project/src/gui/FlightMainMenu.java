693abece019c1ec0d578ee996990795dd5485f57
nicolettepowell <nicolettepowell@localhost>
2022-12-05 16:45:44 -0500

    update

--- CIS-Project/src/gui/FlightMainMenu.java
+++ CIS-Project/src/gui/FlightMainMenu.java
@@ -1,5 +1,33 @@
 package gui;
 
-public class FlightMainMenu {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+public class FlightMainMenu extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = (Parent) FXMLLoader.load(getClass().getResource("WelcomePage.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = null;
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
+
+
+
+
+
+
