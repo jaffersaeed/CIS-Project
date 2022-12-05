e2e74cdd3ddef187221d56fbd63ca13868e3a1da
jaffe <jaffe@DESKTOP-4VTVLKG>
2022-12-05 16:46:46 -0500

    Merge branch 'master' of https://github.com/jaffersaeed/CIS-Project.git

--- CIS-Project/src/gui/Main.java
+++ CIS-Project/src/gui/Main.java
@@ -1,15 +1,20 @@
 package gui;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
-	@Override
+	
 	public void start(Stage primaryStage) {
+		
+		Stage stage = null;
+		
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("WelcomePage.fxml"));
+			Parent root = (Parent) FXMLLoader.load(getClass().getResource("WelcomePage.fxml"));
 			Scene scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
